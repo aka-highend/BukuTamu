75614f6282772e7f8917951cf9ff88fbbb8f0a76
Mohamad Fachrur Ridwan <ridwan.fachrur@gmail.com>
2016-12-08 01:59:26 +0700

    Adding empty classes

---
+++ app/src/main/java/com/rfachrur/bukutamu/adapters/ListTamuAdapter.java
@@ -0,0 +1,8 @@
+package com.rfachrur.bukutamu.adapters;
+
+/**
+ * Created by rfachrur on 12/8/16.
+ */
+
+public class ListTamuAdapter {
+}
---
+++ app/src/main/java/com/rfachrur/bukutamu/model_tamu/Tamu.java
@@ -0,0 +1,8 @@
+package com.rfachrur.bukutamu.model_tamu;
+
+/**
+ * Created by rfachrur on 12/8/16.
+ */
+
+public class Tamu {
+}
